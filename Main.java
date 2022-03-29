0963a42e5683c6b56822d49b3da6afb24ca6b7ae
hvijay-tata <99406041+hvijay-tata@users.noreply.github.com>
2022-03-29 22:08:18 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-      System.out.println("TEST of pipeline to Compile and Run");
+      System.out.println("TEST of pipeline to Compile and Run for college");
     }
 }
