2d2296386090fab4e6bd86d0352cfe0c2137673a
Hardikvj <42835366+Hardikvj@users.noreply.github.com>
2024-05-29 20:06:19 +0530

    Create ecr.tf

---
+++ eks-iac/ecr.tf
@@ -0,0 +1,17 @@
+/*resource "aws_ecr_repository" "mt-widget-recommender" {
+  name                 = "mt-widget-recommender"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_repository" "mt-widget-recommender-admin" {
+  name                 = "mt-widget-recommender-admin"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}*/
